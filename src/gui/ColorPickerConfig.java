2c749495caacc2f235afc3f6f2e33ae5f4252274
macbook <macbook@1145141919810.local>
2019-05-17 18:50:58 +0800

    Before adding new features

--- src/gui/ColorPickerConfig.java
+++ src/gui/ColorPickerConfig.java
@@ -10,15 +10,16 @@ public class ColorPickerConfig {
         colorPicker.setValue(Color.BLACK);
         colorPicker.setOnAction((ActionEvent t) -> {
             int hashCode = colorPicker.getValue().hashCode();
-            int r = hashCode << 16 >> 24;
+            int r = hashCode >> 24;
             if (r < 0)
                 r = 256 + r;
             int g = hashCode << 8 >> 24;
             if (g < 0)
                 g = 256 + g;
-            int b = hashCode >> 24;
+            int b = hashCode << 16 >> 24;
             if (b < 0)
                 b = 256 + b;
+            System.out.println(r + " " + g + " " + b);
             String command = "setColor " + String.valueOf(r) + " " + String.valueOf(g) + " " + String.valueOf(b);
             CliInterpreter.commandProcess(command);
         });
